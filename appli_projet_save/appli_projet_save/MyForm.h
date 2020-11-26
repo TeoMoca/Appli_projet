9fb533f80e27f0c90ea1e4b66ecbcc2ebdfede0a
TeoMoca <teo.montouiscalixte@viacesi.fr>
2020-11-26 17:05:56 +0100

    first commit

---
+++ appli_projet_save/appli_projet_save/MyForm.h
@@ -0,0 +1,248 @@
+#pragma once
+
+namespace connectionmysql {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace MySql::Data::MySqlClient;
+
+
+	/// <summary>
+	/// Description résumée de MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ajoutez ici le code du constructeur
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridView^ dataGridView2;
+	private: System::Windows::Forms::Label^ label4;
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(139, 169);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(100, 33);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Exporter";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(139, 21);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(139, 73);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 22);
+			this->textBox2->TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(139, 120);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 22);
+			this->textBox3->TabIndex = 3;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(76, 24);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(21, 17);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"ID";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(76, 76);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(47, 17);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Article";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(76, 123);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(31, 17);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Prix";
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(277, 24);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->ReadOnly = true;
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(292, 168);
+			this->dataGridView1->TabIndex = 7;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellContentClick);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(22, 169);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(85, 33);
+			this->button2->TabIndex = 8;
+			this->button2->Text = L"visualiser";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->Location = System::Drawing::Point(591, 24);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->RowHeadersWidth = 51;
+			this->dataGridView2->RowTemplate->Height = 24;
+			this->dataGridView2->Size = System::Drawing::Size(297, 168);
+			this->dataGridView2->TabIndex = 9;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(12, 21);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(46, 17);
+			this->label4->TabIndex = 10;
+			this->label4->Text = L"label4";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(949, 225);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->dataGridView2);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"Rangetesclients.net";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ constring = L"datasource=192.168.233.132;port=3306;username=TeoMoca;password=Iammoca*76";
+		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
+		conDataBase->Open();
+		String^ queryString = "INSERT INTO projetdb.articles(id,name,prix) VALUES ( " + Convert::ToInt32(textBox1->Text) + ", '" + textBox2->Text + "', " + Convert::ToInt32(textBox3->Text) + ");";
+		MySqlCommand^ command = gcnew MySqlCommand(queryString, conDataBase);
+		MySqlDataAdapter^ myReader = gcnew MySqlDataAdapter(command);
+		DataTable^ DS = gcnew DataTable();
+		myReader->Fill(DS);
+	}
+
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ constring = L"datasource=192.168.233.132;port=3306;username=TeoMoca;password=Iammoca*76";
+		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
+		conDataBase->Open();
+		String^ queryString = "select * from projetdb.articles;";
+		MySqlCommand^ command = gcnew MySqlCommand(queryString, conDataBase);
+		MySqlDataAdapter^ myAdapter = gcnew MySqlDataAdapter(command);
+		DataTable^ DT = gcnew DataTable();
+		myAdapter->Fill(DT);
+		BindingSource^ source1 = gcnew BindingSource();
+		source1->DataSource = DT;
+		dataGridView1->DataSource = source1;
+		myAdapter->Update(DT);
+	}
+	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+		String^ selected = dataGridView1->Rows[e->RowIndex]->Cells["id"]->Value->ToString();
+		label4->Text = selected;
+		String^ constring = L"datasource=192.168.233.132;port=3306;username=TeoMoca;password=Iammoca*76";
+		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
+		conDataBase->Open();
+		String^ queryString = "select * from projetdb.clients where id = " + selected + ";";
+		MySqlCommand^ command = gcnew MySqlCommand(queryString, conDataBase);
+		MySqlDataAdapter^ myAdapter = gcnew MySqlDataAdapter(command);
+		DataTable^ DD = gcnew DataTable();
+		myAdapter->Fill(DD);
+		BindingSource^ source1 = gcnew BindingSource();
+		source1->DataSource = DD;
+		dataGridView2->DataSource = source1;
+		myAdapter->Update(DD);
+	}
+	};
+}
+
