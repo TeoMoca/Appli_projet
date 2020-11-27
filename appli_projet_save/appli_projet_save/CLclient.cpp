0092658bcf7efea9400f7a6714c58d4cd53805e9
Erwan MARTIN <erwan.martin@viacesi.fr>
2020-11-27 16:52:16 +0100

    puuush

--- appli_projet_save/appli_projet_save/CLclient.cpp
+++ appli_projet_save/appli_projet_save/CLclient.cpp
@@ -1,1 +1,6 @@
 #include "CLclient.h"
+
+CLclient::CLclient() : numClient(0), nom(""), prenom(""), 
+{
+	adresseFact = gcnew array<String^>;
+}
--- appli_projet_save/appli_projet_save/CLclient.h
+++ appli_projet_save/appli_projet_save/CLclient.h
@@ -1,5 +1,19 @@
 #pragma once
+#include<Vector>
+using namespace System;
+
 ref class CLclient
 {
+public:
+	CLclient();
+private:
+	int^ numClient;
+	String^ nom;
+	String^ prenom;
+	array<String^>^ adresseFact;
+	array<String^>^ adresseLivr;
+	String^ birthdate;
+	String^ firstCommande;
+
 };
 
