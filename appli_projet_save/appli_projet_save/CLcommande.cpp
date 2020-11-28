1ae18ac1dc3517080fe50c35b2458544ce58e1c8
Thibaut LIGER HELLARD <thibaut.ligerhellard@viacesi.fr>
2020-11-28 15:49:41 +0100

    on progresse sur le cl commande mais bon, tout ne marche pas lol super je vais manger bisous

--- appli_projet_save/appli_projet_save/CLcommande.cpp
+++ appli_projet_save/appli_projet_save/CLcommande.cpp
@@ -1,1 +1,60 @@
 #include "CLcommande.h"
+#include "pch.h"
+
+CLcommande::CLcommande(String^ refe, int^ num, String^ liv, String^ emi, array<CLpaiement^>^ paie, array<CLcatalog^>^ prod, float^ ht, float^ tva, float^ ttc)
+{
+    ref = refe;
+    numClient = num;
+    dateLivr = liv;
+    dateEmis = emi;
+    paiements = paie;
+    produits = prod;
+    prixHT = ht;
+    TVA = tva;
+    prixTTC = ttc;
+}
+
+String^ CLcommande::getref()
+{
+    return ref;
+}
+
+int^ CLcommande::getnumClient()
+{
+    return numClient;
+}
+
+String^ CLcommande::getdateLivr()
+{
+    return dateLivr;
+}
+
+String^ CLcommande::getdateEmis()
+{
+    return dateEmis;
+}
+
+array<CLpaiement^>^ CLcommande::getpaiements()
+{
+    return paiements;
+}
+
+array<CLcatalog^>^ CLcommande::getproduits()
+{
+    return produits;
+}
+
+float^ CLcommande::getprixHT()
+{
+    return prixHT;
+}
+
+float^ CLcommande::getTVA()
+{
+    return TVA;
+}
+
+float^ CLcommande::getprixTTC()
+{
+    return prixTTC;
+}
--- appli_projet_save/appli_projet_save/CLcommande.h
+++ appli_projet_save/appli_projet_save/CLcommande.h
@@ -3,12 +3,26 @@ ref class CLcommande
 {
 
 	String^ ref, ^dateLivr, ^dateEmis;
+	int^ numClient;
 	array<CLpaiement^>^ paiements;
-	array<CLcatalog^>^ produis;
+	array<CLcatalog^>^ produits;
 	float^ prixHT, ^ TVA, ^ prixTTC;
 
 public: 
 
+	CLcommande(String^, int^, String^, String^, array<CLpaiement^>^, array<CLcatalog^>^, float^, float^, float^);
+
+	String^ getref();
+	int^ getnumClient();
+	String^ getdateLivr();
+	String^ getdateEmis();
+	array<CLpaiement^>^ getpaiements();
+	array<CLcatalog^>^ getproduits();
+	float^ getprixHT();
+	float^ getTVA();
+	float^ getprixTTC();
+
+	
 
 
 };
