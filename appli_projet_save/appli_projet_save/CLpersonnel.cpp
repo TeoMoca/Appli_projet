2e5d27c4dee1c50d80233baf8d605053b12e26eb
Erwan MARTIN <erwan.martin@viacesi.fr>
2020-11-29 16:07:36 +0100

    CLpersonnel

--- appli_projet_save/appli_projet_save/CLpersonnel.cpp
+++ appli_projet_save/appli_projet_save/CLpersonnel.cpp
@@ -1,2 +1,72 @@
 #include "CLpersonnel.h"
 #include "pch.h"
+
+CLpersonnel::CLpersonnel():nom(""), prenom(""), dateEmbauche(""), IDsup(0)
+{
+    //throw gcnew System::NotImplementedException();
+    adresse = nullptr;
+}
+
+String^ CLpersonnel::getnom()
+{
+    //throw gcnew System::NotImplementedException();
+    return nom;
+}
+
+void CLpersonnel::setnom(String^ str)
+{
+    //throw gcnew System::NotImplementedException();
+    nom = str;
+}
+
+String^ CLpersonnel::getprenom()
+{
+    //throw gcnew System::NotImplementedException();
+    return prenom;
+}
+
+void CLpersonnel::setprenom(String^ str)
+{
+    //throw gcnew System::NotImplementedException();
+    prenom = str;
+}
+
+String^ CLpersonnel::getdateEmbauche()
+{
+    //throw gcnew System::NotImplementedException();
+    return dateEmbauche;
+}
+
+void CLpersonnel::setdateEmbauche(String^ str)
+{
+    //throw gcnew System::NotImplementedException();
+    dateEmbauche = str;
+}
+
+CLadresse^ CLpersonnel::getadresse()
+{
+    //throw gcnew System::NotImplementedException();
+    return adresse;
+}
+
+void CLpersonnel::setadresse(array<String^>^ nouvelleAdresse)
+{
+    //throw gcnew System::NotImplementedException();
+    adresse = gcnew CLadresse;
+    adresse->setnumeroRue(nouvelleAdresse[0]);
+    adresse->setville(nouvelleAdresse[1]);
+    adresse->setcodePostal(nouvelleAdresse[2]);
+    adresse->setappartNum(nouvelleAdresse[3]);
+}
+
+int^ CLpersonnel::getIDsup()
+{
+    //throw gcnew System::NotImplementedException();
+    return IDsup;
+}
+
+void CLpersonnel::setIDsup(int^ nb)
+{
+    //throw gcnew System::NotImplementedException();
+    IDsup = nb;
+}
--- appli_projet_save/appli_projet_save/CLpersonnel.h
+++ appli_projet_save/appli_projet_save/CLpersonnel.h
@@ -1,5 +1,27 @@
 #pragma once
+#include "pch.h"
+using namespace System;
+
 ref class CLpersonnel
 {
+public:
+	CLpersonnel();
+	String^ getnom();
+	void setnom(String^);
+	String^ getprenom();
+	void setprenom(String^);
+	String^ getdateEmbauche();
+	void setdateEmbauche(String^);
+	CLadresse^ getadresse();
+	void setadresse(array<String^>^);
+	int^ getIDsup();
+	void setIDsup(int^);
+
+private:
+	String^ nom;
+	String^ prenom;
+	String^ dateEmbauche;
+	CLadresse^ adresse;
+	int^ IDsup;
 };
 
