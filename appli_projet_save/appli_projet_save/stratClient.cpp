111f5913c16365cff30d845013a24f35e01ad21f
TeoMoca <teo.montouiscalixte@viacesi.fr>
2020-12-01 09:35:56 +0100

    crud 4

--- appli_projet_save/appli_projet_save/stratClient.cpp
+++ appli_projet_save/appli_projet_save/stratClient.cpp
@@ -4,7 +4,6 @@ using namespace appliprojet;
 
 void stratClient::create(array<array<String^>^>^ adresse_fact, array<array<String^>^>^ adresse_liv,String^ nom, String^ prenom, String^ birthdate) {
 	CLclient obj;
-	String^ test;
 	conDataBase->Open();
 	obj.setnom(nom);
 	obj.setprenom(prenom);
@@ -16,16 +15,18 @@ void stratClient::create(array<array<String^>^>^ adresse_fact, array<array<Strin
 	cmdidclient = "select id from projetpoo.CLIENT where NOM = '" + nom + "' and PRENOM = '" + prenom + "' and BIRTHDATE = '" + birthdate + "';";
 	MySqlDataReader^ myreader = command->ExecuteReader();
 	myreader->Read();
-	String^ idclient = myreader->GetString(0);
+	int^ idclient = myreader->GetInt32(0);
 
 	//MyForm->collection->label4->Text = idclient;
 };
-void stratClient::read() {
-
+void stratClient::read(String^ nom, String^ prenom, String^ birthdate) {
+	CLclient read;
+	cmdclient = "select * from projetpoo.CLIENT where NOM = '" + nom + "' and PRENOM = '" + prenom + "' and BIRTHDATE = '" + birthdate + "';";
 };
 void stratClient::update() {
 
 };
-void stratClient::suppr() {
-
+void stratClient::suppr(String^ nom, String^ prenom, String^ birthdate) {
+	CLclient suppr;
+	cmdclient = "delete from projetpoo.CLIENT where NOM = '" + nom + "' and PRENOM = '" + prenom + "' and BIRTHDATE = '" + birthdate + "';";
 };
--- appli_projet_save/appli_projet_save/stratClient.h
+++ appli_projet_save/appli_projet_save/stratClient.h
@@ -5,9 +5,9 @@ ref class stratClient:public stratCRUD
 {
 public:
 	void create(array<array<String^>^>^ , array<array<String^>^>^ ,String^ , String^, String^) override;
-	void read() override ;
+	void read(String^ nom, String^ prenom, String^ birthdate) override ;
 	void update() override;
-	void suppr() override;
+	void suppr(String^ nom, String^ prenom, String^ birthdate) override;
 	
 };
 
